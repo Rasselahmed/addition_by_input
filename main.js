886c725a5105cb5487bf5eef5a7b597d93d48d21
Rassel Ahmed <94746949+Rasselahmed@users.noreply.github.com>
2022-02-04 18:54:01 +0600

    Update main.js
    edit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function add(number1, number2) {
     var r = parseInt(number1) + parseInt(number2);
-    document.write(r);
+    document.getElementById("output").innerhtml = r;
     return r;
 }
 add();
