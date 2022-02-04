fbd928681a11ead3a105d9dc6a28ec62f81c6fc1
Rassel Ahmed <94746949+Rasselahmed@users.noreply.github.com>
2022-02-04 19:20:37 +0600

    Add files via upload
    sadsaf

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 function add(number1, number2) {
     var r = parseInt(number1) + parseInt(number2);
-    document.getElementById("output").innerhtml = r;
-    return r;
+  document.getElementById("chailam").innerHTML = r;
 }
-add();
